9861a816f42dc51bcc7361bb2d661589da6a0b47
Vinícius Lourenço <vinyygamerlol@gmail.com>
2018-08-31 18:55:57 -0300

    Implementando alguns métodos do LaunchMVP

--- app/src/main/java/wiseonesoft/com/br/drwise/LaunchActivity.java
+++ app/src/main/java/wiseonesoft/com/br/drwise/LaunchActivity.java
@@ -1,14 +1,16 @@
 package wiseonesoft.com.br.drwise;
 
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 
 import wiseonesoft.com.br.drwise.interfaces.LaunchMVP;
 import wiseonesoft.com.br.drwise.presenters.LaunchPresenter;
 
-public class LaunchActivity extends Activity  implements LaunchMVP.View{
+public class LaunchActivity extends Activity  implements LaunchMVP.View {
 
-    LaunchMVP.Presenter presenter;
+     LaunchMVP.Presenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +19,22 @@ public class LaunchActivity extends Activity  implements LaunchMVP.View{
         setContentView(R.layout.activity_launch);
 
         presenter = new LaunchPresenter(this);
+
+        new Handler().postDelayed(presenter::onSplashComplete, 1000);
+    }
+
+    @Override
+    public Context getContext() {
+        return this.getBaseContext();
+    }
+
+    @Override
+    public void showLoginActivity() {
+
+    }
+
+    @Override
+    public void showMainActivity() {
+
     }
 }
--- app/src/main/java/wiseonesoft/com/br/drwise/presenters/LaunchPresenter.java
+++ app/src/main/java/wiseonesoft/com/br/drwise/presenters/LaunchPresenter.java
@@ -1,12 +1,30 @@
 package wiseonesoft.com.br.drwise.presenters;
 
+import wiseonesoft.com.br.drwise.R;
 import wiseonesoft.com.br.drwise.interfaces.LaunchMVP;
+import wiseonesoft.com.br.drwise.security.SecurityPreferences;
 
 public class LaunchPresenter implements LaunchMVP.Presenter {
 
+    private SecurityPreferences securityPreferences;
     private LaunchMVP.View view;
 
     public LaunchPresenter(LaunchMVP.View view) {
         this.view = view;
+        this.securityPreferences = new SecurityPreferences(view.getContext());
+    }
+
+    @Override
+    public void onSplashComplete() {
+        if(hasLogged())
+            view.showMainActivity();
+        else
+            view.showLoginActivity();
+
+    }
+
+    @Override
+    public boolean hasLogged() {
+        return !securityPreferences.getString(view.getString(R.string.api_token_key)).isEmpty();
     }
 }
