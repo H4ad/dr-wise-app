a958040aab356e10971dbc8df1014262ea14a764
Vinícius Lourenço <vinyygamerlol@gmail.com>
2018-08-31 18:55:26 -0300

    Atualizando LaunchMVP

--- app/src/main/java/wiseonesoft/com/br/drwise/interfaces/LaunchMVP.java
+++ app/src/main/java/wiseonesoft/com/br/drwise/interfaces/LaunchMVP.java
@@ -1,12 +1,44 @@
 package wiseonesoft.com.br.drwise.interfaces;
 
+import android.content.Context;
+
 public interface LaunchMVP {
 
     interface View {
 
+        /**
+         * Retorna o contexto da View
+         */
+        Context getContext();
+
+        /**
+         * Retorna uma String do arquivo strings.xml
+         */
+        String getString(int id);
+
+        /**
+         * Exibe a Activity de Login
+         */
+        void showLoginActivity();
+
+
+        /**
+         * Exibe a Activity principal
+         */
+        void showMainActivity();
     }
 
     interface Presenter {
+
+        /**
+         * Define um comportamento quando terminar a Splash Screen
+         */
+        void onSplashComplete();
+
+        /**
+         * Verifica se o usuário está logado
+         */
+        boolean hasLogged();
 
     }
 }
