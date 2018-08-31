3ca31e3e6fae5bc60142d100725af6ad81bf9457
Vinícius Lourenço <vinyygamerlol@gmail.com>
2018-08-31 17:42:51 -0300

    Inicializando aplicativo

---
+++ app/src/main/java/wiseonesoft/com/br/drwise/LaunchActivity.java
@@ -0,0 +1,13 @@
+package wiseonesoft.com.br.drwise;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LaunchActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+    }
+}
