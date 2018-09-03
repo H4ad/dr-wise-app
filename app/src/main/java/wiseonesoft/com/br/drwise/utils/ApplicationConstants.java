2da8efba53ce2ec9311882950e78a97dd31d8645
Vinícius Lourenço <vinyygamerlol@gmail.com>
2018-09-02 21:25:58 -0300

    Adicionando classe para segurar todas as constantes

---
+++ app/src/main/java/wiseonesoft/com/br/drwise/utils/ApplicationConstants.java
@@ -0,0 +1,7 @@
+package wiseonesoft.com.br.drwise.utils;
+
+public class ApplicationConstants {
+
+    public final static String API_URL = "https://wiseonesoft.com/api/";
+    public final static String EMPTY_STRING = "";
+}
